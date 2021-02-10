eb5b433b96ff1a134c5c41b695368385d4db71b6
Louis Coilliot <louis.coilliot@gmail.com>
2021-02-10 19:17:20 +0100

    el8

--- vmcreate/sandbox01.sh
+++ vmcreate/sandbox01.sh
@@ -1,5 +1,5 @@
 #url=http://centos.mirrors.ovh.net/ftp.centos.org/7/os/x86_64/
-url=http://192.168.0.10/repos/el7/x86_64/
+url=http://192.168.0.10/repos/el8/x86_64/
 ip=192.168.0.17
 kspath=$HOME/Documents/labs/provisioning/ks/sandbox01.cfg
 disk1=10G
